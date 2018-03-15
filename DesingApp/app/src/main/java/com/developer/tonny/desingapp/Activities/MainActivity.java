c617316e603e123b51ee67e7333e5829d547f1fc
tonnyxp20 <37387727+tonnyxp20@users.noreply.github.com>
2018-03-14 18:53:01 -0600

    Desing App

---
+++ DesingApp/app/src/main/java/com/developer/tonny/desingapp/Activities/MainActivity.java
@@ -0,0 +1,79 @@
+package com.developer.tonny.desingapp.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.developer.tonny.desingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.menu_main:
+
+                        break;
+
+                    case R.id.menu_emergency:
+                        Intent e = new Intent(getApplicationContext(), EmergencyActivity.class);
+                        startActivity(e);
+                        break;
+
+                    case R.id.menu_medicines:
+                        Intent m = new Intent(getApplicationContext(), MedicinesActivity.class);
+                        startActivity(m);
+                        break;
+
+                    case R.id.menu_citation:
+                        Intent c = new Intent(getApplicationContext(), CitationActivity.class);
+                        startActivity(c);
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ DesingApp/app/src/main/java/com/developer/tonny/desingapp/Activities/MedicinesActivity.java
@@ -0,0 +1,15 @@
+package com.developer.tonny.desingapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.developer.tonny.desingapp.R;
+
+public class MedicinesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicines);
+    }
+}
